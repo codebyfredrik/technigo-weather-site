d3a83856ba8e8702ebe02970ac42d7fb31a2ce84
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2020-04-14 15:27:38 +0200

    Update forecast time

--- weather.js
+++ weather.js
@@ -1,54 +1,54 @@
 const APIKEY = '3d9e5059943c355e05df6f5850ac47d3';
 
 const getWeather = async (longitude, latitude) => {
-	const base = 'https://api.openweathermap.org/data/2.5/weather';
-	const query = `?lat=${latitude}&lon=${longitude}&units=metric&APPID=${APIKEY}`;
+  const base = 'https://api.openweathermap.org/data/2.5/weather';
+  const query = `?lat=${latitude}&lon=${longitude}&units=metric&APPID=${APIKEY}`;
 
-	const response = await fetch(base + query);
-	const data = await response.json();
+  const response = await fetch(base + query);
+  const data = await response.json();
 
-	return data;
+  return data;
 };
 
 const getForecast = async (longitude, latitude) => {
-	const base = 'https://api.openweathermap.org/data/2.5/forecast';
-	const query = `?lat=${latitude}&lon=${longitude}&units=metric&cnt=${40}&APPID=${APIKEY}`;
+  const base = 'https://api.openweathermap.org/data/2.5/forecast';
+  const query = `?lat=${latitude}&lon=${longitude}&units=metric&cnt=${40}&APPID=${APIKEY}`;
 
-	const response = await fetch(base + query);
-	const data = await response.json();
+  const response = await fetch(base + query);
+  const data = await response.json();
 
-	return data;
+  return data;
 };
 
-const displayCurrentWeather = weather => {
-	const currentWeather = document.querySelector('.weather-details-current');
-	let backgroundColor = '';
+const displayCurrentWeather = (weather) => {
+  const currentWeather = document.querySelector('.weather-details-current');
+  let backgroundColor = '';
 
-	// Destructuring weather object
-	const {
-		main: { temp: temperature, humidity, pressure, temp_min, temp_max },
-		sys: { sunrise, sunset },
-		wind,
-		displayName
-	} = weather;
+  // Destructuring weather object
+  const {
+    main: { temp: temperature, humidity, pressure, temp_min, temp_max },
+    sys: { sunrise, sunset },
+    wind,
+    displayName,
+  } = weather;
 
-	// Change background colors based on current temperature
-	if (temperature > 20) {
-		backgroundColor = 'linear-gradient(to bottom right, #ffc500, #c21500)';
-	} else if (temperature >= 10) {
-		backgroundColor = 'linear-gradient(to bottom right, #dce35b, #247026)';
-	} else {
-		backgroundColor = 'linear-gradient(to bottom right, #1cb5e0, #000046)';
-	}
+  // Change background colors based on current temperature
+  if (temperature > 20) {
+    backgroundColor = 'linear-gradient(to bottom right, #ffc500, #c21500)';
+  } else if (temperature >= 10) {
+    backgroundColor = 'linear-gradient(to bottom right, #dce35b, #247026)';
+  } else {
+    backgroundColor = 'linear-gradient(to bottom right, #1cb5e0, #000046)';
+  }
 
-	document.body.style.background = backgroundColor;
+  document.body.style.background = backgroundColor;
 
-	// Update current weather template
-	currentWeather.innerHTML = `
+  // Update current weather template
+  currentWeather.innerHTML = `
   <p class="current-temperature">${Math.round(temperature)} &deg;C</p>
   <img src="https://openweathermap.org/img/wn/${
-		weather.weather[0].icon
-	}@2x.png" alt="weather condition">
+    weather.weather[0].icon
+  }@2x.png" alt="weather condition">
   <p class="location">${displayName}</p>
   <p class="overall-weather">${capitalize(weather.weather[0].description)}</p>
 	<p class="sunrise-sunset">
@@ -82,16 +82,16 @@ const displayCurrentWeather = weather => {
   `;
 };
 
-const displayForecast5days = forecast => {
-	const forecastWeatherList = document.querySelector('#forecast-list');
+const displayForecast5days = (forecast) => {
+  const forecastWeatherList = document.querySelector('#forecast-list');
 
-	// Filtering out one forcast out of six, per day.
-	const filteredForecast = forecast.list.filter(item => {
-		return convertUnixToTime(item.dt) === '13:00';
-	});
+  // Filtering out one forcast out of six, per day.
+  const filteredForecast = forecast.list.filter((item) => {
+    return convertUnixToTime(item.dt) === '11:00';
+  });
 
-	// Reset 24-hour forecast template with headings before generating the forecast template
-	forecastWeatherList.innerHTML = `
+  // Reset 24-hour forecast template with headings before generating the forecast template
+  forecastWeatherList.innerHTML = `
   <li>
     <div class="forecast-day-heading"></div>
     <div class="forecast-day-heading"></div>
@@ -101,49 +101,49 @@ const displayForecast5days = forecast => {
     <div class="forecast-day-heading">Temp.</div>
   </li>`;
 
-	// Generate 5-day forecast weather
-	filteredForecast.forEach(item => {
-		forecastWeatherList.innerHTML += `
+  // Generate 5-day forecast weather
+  filteredForecast.forEach((item) => {
+    forecastWeatherList.innerHTML += `
     <li>
       <div class="forecast-day">${convertUnixToDay(item.dt)}</div>
       <div>
         <img class="icon" src="https://openweathermap.org/img/wn/${
-					item.weather[0].icon
-				}@2x.png" alt="weather condition">
+          item.weather[0].icon
+        }@2x.png" alt="weather condition">
       </div>
       <div class="forecast-day">${item.main.humidity}%</div>
       <div class="forecast-day">${item.main.pressure} hPa</div>
       <div class="forecast-day">${
-				item.wind.deg
-			}&deg; @ ${item.wind.speed.toFixed(1)} m/s</div>
+        item.wind.deg
+      }&deg; @ ${item.wind.speed.toFixed(1)} m/s</div>
       <div class="forecast-day">${Math.round(item.main.temp)} &deg;C</div>
     </li>`;
-	});
+  });
 };
 
 const displayForecast24hours = (weather, forecast) => {
-	const forecastDaily = document.querySelector('#daily-forecast-list');
+  const forecastDaily = document.querySelector('#daily-forecast-list');
 
-	const currentForecastItems = forecast.list.filter(items => {
-		return (
-			convertUnixToDateTime(items.dt) <
-			convertUnixToDateTime(weather.dt).add(1, 'days')
-		);
-	});
+  const currentForecastItems = forecast.list.filter((items) => {
+    return (
+      convertUnixToDateTime(items.dt) <
+      convertUnixToDateTime(weather.dt).add(1, 'days')
+    );
+  });
 
-	// Reset 24-hour forecast template
-	forecastDaily.innerHTML = ``;
+  // Reset 24-hour forecast template
+  forecastDaily.innerHTML = ``;
 
-	// Generate 24-hour forecast template
-	currentForecastItems.forEach(item => {
-		forecastDaily.innerHTML += `
+  // Generate 24-hour forecast template
+  currentForecastItems.forEach((item) => {
+    forecastDaily.innerHTML += `
       <li>
         <p>${convertUnixToTime(item.dt)}</p>
         <p>${Math.round(item.main.temp)} &deg;C</p>
         <img class="icon" src="https://openweathermap.org/img/wn/${
-					item.weather[0].icon
-				}@2x.png" alt="weather condition">
+          item.weather[0].icon
+        }@2x.png" alt="weather condition">
       </li>
     `;
-	});
+  });
 };
